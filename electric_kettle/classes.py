7e29950ffc33b1f961695fee2e2ee3a17a42225e
Vladiss <vladislav.kulakov.2002@mail.ru>
2023-02-16 21:07:45 +0300

    final

--- electric_kettle/classes.py
+++ electric_kettle/classes.py
@@ -1,6 +1,5 @@
 import configparser
 
-
 config = configparser.ConfigParser()  # создаём объект парсера
 config.read("settings.ini", encoding="utf-8")
 
@@ -12,3 +11,8 @@ class Kettle:
         self.shutdown_temp = int(config["Vitek"]["shutdown_temp"])
         self.water_volume = float(config["Vitek"]["water_volume"])
         self.default_water_temp = int(config["Vitek"]["default_water_temp"])
+
+    def __str__(self):
+        return f'Время закипания воды - {self.boil_time} секунд, ' \
+               f'температура автоматического выключения - {self.shutdown_temp} °C, ' \
+               f'количество воды - {self.water_volume}'
